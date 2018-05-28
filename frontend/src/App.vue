96f45167ca1eda85156521f53e7457cbe3aae8fe
cjc <shcjc96@gmail.com>
2018-05-28 12:06:06 +0800

    fix ui bugs
    add recursive solution

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -18,14 +18,14 @@
         <b-col cols='3' class='right-line'>
           <b-row v-for="(item,index) in types" :key="index" class='custom-input'>
             <b-col cols="3"><label :for="`input-${item}`">{{ item }}:</label></b-col>
-            <b-col cols="9"><b-form-input :id="`input-${item}`" v-model="input_info[item]" :value="input_info[item]"></b-form-input></b-col>
+            <b-col cols="9"><b-form-input :id="`input-${item}`" v-model="input_info[item]" :value="input_info[item]" @change="redraw"></b-form-input></b-col>
           </b-row>
           <b-row class='custom-input'>
-            <b-col cols="3"><label :for="`input-Epsilon`">Epsilon:</label></b-col>
-            <b-col cols="9"><b-form-input :id="`input-Epsilon`" v-model="input_info['Epsilon']" :value="input_info['Epsilon']" :type="'range'" min=0.05 max=1 step=0.05></b-form-input></b-col>
+            <b-col cols="3"><label :for="`input-Epsilon`">Deviation:</label></b-col>
+            <b-col cols="9"><b-form-input :id="`input-Epsilon`" v-model="input_info['Epsilon']" :value="input_info['Epsilon']" :type="'range'" min=1 max=20 step=1></b-form-input></b-col>
           </b-row>
           <b-row class='custom-input'>
-            <p>{{ input_info['Epsilon'] }}</p>
+            <p>{{ input_info['Epsilon'] }}%</p>
           </b-row>
           <b-button v-on:click="submit" size='lg' variant='success' class='custom-input'>Submit</b-button>
           <div class='bottom-left'>
@@ -87,16 +87,24 @@ export default {
       clickStyle : 0,
       firstClick: {x: 0, y: 0},
       realAns: 0,
-      noisyAns: 0
+      noisyAns: 0,
+      saveLife: {
+        width:0,
+        height: 0,
+        clientX: 0,
+        clientY: 0
+      }
     }
   },
   methods: {
     linkGen (pageNum) {
       return this.links[pageNum - 1]
     },
+
     pageGen (pageNum) {
       return this.links[pageNum - 1].slice(1)
     },
+
     draw (event) {
       let canvas = document.getElementById("canvas");
       let ctx = canvas.getContext('2d');
@@ -105,8 +113,11 @@ export default {
       let tmp = this.img_scale[this.currentPage];
       let tmp_x = tmp.xmin + (tmp.xmax - tmp.xmin) * (event.offsetX / event.path[0].width);
       let tmp_y = tmp.ymin + (tmp.ymax - tmp.ymin) * (event.offsetY / event.path[0].height);
-      console.log(tmp_x, tmp_y);
-      console.log(this.input_info);
+      this.saveLife.width = event.path[0].width;
+      this.saveLife.height = event.path[0].height;
+      this.saveLife.clientX = event.clientX;
+      this.saveLife.clientY = event.clientY;
+      console.log(event.clientX, event.clientY);
       if (this.clickStyle == 0) {
         console.log("first click");
         this.firstClick.x = event.clientX;
@@ -126,16 +137,42 @@ export default {
       }
       this.clickStyle = 1 - this.clickStyle;
     },
+
     submit () {
       let host = 'http://localhost:8888/';
       let currentName = this.links[this.currentPage - 1].slice(1);
-      let url = host + `get_ans?location=${currentName}&xmin=${this.input_info.xmin}&ymin=${this.input_info.ymin}&xmax=${this.input_info.xmax}&ymax=${this.input_info.ymax}&epsilon=${this.input_info.Epsilon}`;
+      let url = host + `get_ans?location=${currentName}&xmin=${this.input_info.xmin}&ymin=${this.input_info.ymin}&xmax=${this.input_info.xmax}&ymax=${this.input_info.ymax}&epsilon=${1.0/this.input_info.Epsilon}`;
       fetch(url)
       .then(res => res.json())
       .then(res => {
         this.realAns = res.realAns;
         this.noisyAns = res.noisyAns;
       });
+    },
+
+    redraw () {
+      console.log("redraw");
+      let canvas = document.getElementById("canvas");
+      canvas.width = window.innerWidth;
+      canvas.height = window.innerHeight;
+      let tmp = this.img_scale[this.currentPage];
+      let relocation = {
+        xmin : 0,
+        ymin : 0,
+        xmax : 0,
+        ymax : 0
+      };
+      relocation.xmin = (this.input_info.xmin - tmp.xmin) / (tmp.xmax - tmp.xmin) * this.saveLife.width;
+      relocation.ymin = (this.input_info.ymin - tmp.ymin) / (tmp.ymax - tmp.ymin) * this.saveLife.height;
+      relocation.xmax = (this.input_info.xmax - tmp.xmin) / (tmp.xmax - tmp.xmin) * this.saveLife.width;
+      relocation.ymax = (this.input_info.ymax - tmp.ymin) / (tmp.ymax - tmp.ymin) * this.saveLife.height;
+      console.log(relocation);
+      let ctx = canvas.getContext('2d');
+      ctx.beginPath();
+      ctx.rect(this.saveLife.clientX + relocation.xmin, this.saveLife.clientY + relocation.ymin, relocation.xmax - relocation.xmin, relocation.ymax - relocation.ymin);
+      ctx.lineWidth = 7;
+      ctx.strokeStyle = "green";
+      ctx.stroke();
     }
   }
 }
